2611278b4b1ec66efa02be8efdfed3774688ba92
Maagio <jmunck.nielsen@gmail.com>
2018-12-12 10:53:17 +0100

    Add files via upload

---
+++ konsol.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./test");
+test_1.noPost();
+//# sourceMappingURL=konsol.js.map
---
+++ konsol.ts
@@ -0,0 +1,3 @@
+import {noPost} from "./test";
+
+noPost();
