6e4153496975e92bff9ee7d8aabc23a834faf894
Maagio <jmunck.nielsen@gmail.com>
2018-12-12 10:49:01 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,29 +1,39 @@
-//import {start} from "./test";
-//import {log} from "util";
-import {start} from "./test";
+import {start, testing} from "./test";
 
 let path = require("path");
 let express = require("express");
+let bodyParser = require("body-parser");
 let logger = require("morgan");
 let app = express();
 
 app.use(logger("dev"));
-
 app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.json({limit: "10mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "10mb", extended: true}));
 
 app.get("/", function (req, res) {
     res.sendFile("index.html", {root: __dirname});
-})
+});
 
-app.post("/", async function(req, res) {
+app.post("/training", async function(req, res) {
     try{
-        console.log(req.body);
+        console.log("Træning begyndt");
         await start();
+        console.log("Træning fuldført");
     } catch (e) {
         console.log(e.message);
     }
 });
+
+app.post("/testing", async  function(req, res) {
+    try{
+        console.log("Testing påbegyndt");
+        //console.log(req.body.image);
+        await testing();
+    } catch (e) {
+        console.log(e.message);
+    }
+});
+
 app.listen(3000);
 console.log("listening on port 3000");
-
-//start();
