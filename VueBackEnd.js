d31074572a3a2dc740e8afd58f31a639fcf5a440
Maagio <jmunck.nielsen@gmail.com>
2018-12-12 10:45:41 +0100

    Update VueBackEnd.js

--- VueBackEnd.js
+++ VueBackEnd.js
@@ -1,7 +1,9 @@
 new Vue({
     el: '#app',
     data: {
-        image: ''
+        image: '',
+        testImage: "",
+        number: "0"
     },
     methods: {
         onFileChange(e) {
@@ -18,16 +20,37 @@ new Vue({
             reader.onload = (e) => {
                 vm.image = e.target.result;
             };
+            //reader.onload = (e) => this.$emit("load", e.target.result);
+                //vm.image = e.target.result;
+            //this.testImage = reader.readAsArrayBuffer(vm.image);
             reader.readAsDataURL(file);
+            this.image = vm.image;
+            //console.log(this.image);
         },
         removeImage: function (e) {
             this.image = '';
+        },
+        train: function() {
+            // POST request
+            this.$http.post('/training')
+            //method: 'POST'
+                .then(response => {
+                    console.log('ok');
+                });
+        },
+        test: function() {
+            // POST request
+            //let formData = new FormData();
+            //formData.append("myFile", this.number, this.number.name);
+            //this.testImage = reader.readAsArrayBuffer(this.image);
+            if (number == 0) {
+                this.$http.post('/testing')
+                //method: 'POST'
+                    .then(response => {
+                        console.log('ok');
+                    });
+                number = 1;
+            }
         }
-    }
-})
-var test = new Vue({
-    el: "#test",
-    data: {
-        counter: 0
     }
 })
